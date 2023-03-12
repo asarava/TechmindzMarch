32ceb831fe3d74521fa84a7139e17f4af0a20280
Dona1705 <126848486+Dona1705@users.noreply.github.com>
2023-03-12 16:19:19 +0530

    project

---
+++ ProjectFinal/Final_Project/src/main/java/com/pages/SignUpPage.java
@@ -0,0 +1,33 @@
+package com.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.base.TestBase;
+import com.util.TestUtil;
+
+public class SignUpPage extends TestBase {
+	@FindBy(id = "sign-username")
+	WebElement sUsername;
+
+	@FindBy(id = "sign-password")
+	WebElement sPassword;
+
+	@FindBy(xpath = "//button[text()='Sign up']")
+	WebElement signUpBtn;
+
+	public SignUpPage() {
+		PageFactory.initElements(driver, this);
+
+	}
+
+	public void signUp(String un, String pwd) {
+
+		TestUtil.sendKeys(driver, sUsername, un);
+		TestUtil.sendKeys(driver, sPassword, pwd);
+
+		TestUtil.JSClick(driver, signUpBtn);
+	}
+
+}
